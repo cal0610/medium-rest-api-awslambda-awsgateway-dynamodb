72562e3377e11b3ee4b1370c3cf0489152f26e3a
Calvin Pang <cal.pang123@gmail.com>
2022-11-01 21:05:00 +1100

    Add lambda function

--- lib/myStack.ts
+++ lib/myStack.ts
@@ -1,21 +1,23 @@
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import {
   AttributeType,
   BillingMode,
   Table,
   TableClass,
-} from "aws-cdk-lib/aws-dynamodb";
+} from 'aws-cdk-lib/aws-dynamodb';
+import { join } from 'path';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class MyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const api = new apigateway.RestApi(this, "test-api", {
-      description: "Test project",
+    const api = new apigateway.RestApi(this, 'test-api', {
+      description: 'Test project',
       deployOptions: {
-        stageName: "dev",
+        stageName: 'dev',
       },
       defaultCorsPreflightOptions: {
         allowOrigins: apigateway.Cors.ALL_ORIGINS, // replace with calling origin
@@ -23,11 +25,45 @@ export class MyStack extends cdk.Stack {
       },
     });
 
-    const storesTable = new Table(this, "stores-table", {
-      partitionKey: { name: "id", type: AttributeType.STRING },
+    const storesTable = new Table(this, 'stores-table', {
+      partitionKey: { name: 'id', type: AttributeType.STRING },
       billingMode: BillingMode.PAY_PER_REQUEST,
       tableClass: TableClass.STANDARD_INFREQUENT_ACCESS,
-      tableName: "stores",
+      tableName: 'stores',
     });
+
+    const nodejsProps: NodejsFunctionProps = {
+      depsLockFilePath: join(__dirname, '..', 'package-lock.json'),
+      environment: {
+        STORE_PRIMARY_KEY: 'id',
+        STORE_TABLE_NAME: storesTable.tableName,
+      }
+    };
+
+    const getOneLambda = new NodejsFunction(this, 'getOneStoreFunction', {
+      entry: join(__dirname, '..', 'get-one.ts'),
+      ...nodejsProps,
+    });
+
+    const getAllLambda = new NodejsFunction(this, 'getAllStoresFunction', {
+      entry: join(__dirname, '..', 'get-all.ts'),
+      ...nodejsProps,
+    });
+
+    const createOneLambda = new NodejsFunction(this, 'createStoreFunction', {
+      entry: join(__dirname, '..', 'create.ts'),
+      ...nodejsProps,
+    });
+    
+    const updateOneLambda = new NodejsFunction(this, 'updateStoreFunction', {
+      entry: join(__dirname, '..', 'update-one.ts'),
+      ...nodejsProps,
+    });
+
+    const deleteOneLambda = new NodejsFunction(this, 'deleteStoreFunction', {
+      entry: join(__dirname, '..', 'delete-one.ts'),
+      ...nodejsProps,
+    });
+
   }
 }
