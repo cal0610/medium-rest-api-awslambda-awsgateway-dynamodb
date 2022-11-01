4dee5db37d943f9a22bdf8a5841f44046a4981dc
Calvin Pang <cal.pang123@gmail.com>
2022-11-01 20:46:53 +1100

    add dynamodb table

--- lib/myStack.ts
+++ lib/myStack.ts
@@ -1,20 +1,33 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import {
+  AttributeType,
+  BillingMode,
+  Table,
+  TableClass,
+} from "aws-cdk-lib/aws-dynamodb";
 
 export class MyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const api = new apigateway.RestApi(this, 'test-api', {
-      description: 'Test project',
+    const api = new apigateway.RestApi(this, "test-api", {
+      description: "Test project",
       deployOptions: {
-        stageName: 'dev',
+        stageName: "dev",
       },
       defaultCorsPreflightOptions: {
         allowOrigins: apigateway.Cors.ALL_ORIGINS, // replace with calling origin
-        allowMethods: apigateway.Cors.ALL_METHODS
+        allowMethods: apigateway.Cors.ALL_METHODS,
       },
+    });
+
+    const storesTable = new Table(this, "stores-table", {
+      partitionKey: { name: "id", type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      tableClass: TableClass.STANDARD_INFREQUENT_ACCESS,
+      tableName: "stores",
     });
   }
 }
