9b7ceb15cc47989de358d3042105c37248093c18
Calvin Pang <cal.pang123@gmail.com>
2022-11-01 20:40:08 +1100

    Add rest api

--- lib/myStack.ts
+++ lib/myStack.ts
@@ -1,11 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class MyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
+    const api = new apigateway.RestApi(this, 'test-api', {
+      description: 'Test project',
+      deployOptions: {
+        stageName: 'dev',
+      },
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS, // replace with calling origin
+        allowMethods: apigateway.Cors.ALL_METHODS
+      },
+    });
   }
 }
