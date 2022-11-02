68e1c9cb7433ac639975df56074ef788f134f6b8
Calvin Pang <cal.pang123@gmail.com>
2022-11-02 19:50:55 +1100

    Cognito authorizer

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-# Welcome to your CDK TypeScript project
+# Cognito user pool authorizer example project
 
-This is a blank project for CDK development with TypeScript.
-
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
-
-## Useful commands
-
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+## Make sure to verify the created user
+aws cognito-idp admin-set-user-password  --user-pool-id ap-southeast-2_1EEtzwUa0 --username medium-admin-user --password Medium-admin-user123! --permanent
--- lib/myStack.ts
+++ lib/myStack.ts
@@ -1,15 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import {
-  AttributeType,
-  BillingMode,
-  Table,
-  TableClass,
-} from 'aws-cdk-lib/aws-dynamodb';
+import { AttributeType, BillingMode, Table, TableClass } from 'aws-cdk-lib/aws-dynamodb';
 import { join } from 'path';
 import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
+import { OAuthScope } from 'aws-cdk-lib/aws-cognito';
 
 export class MyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -26,11 +24,66 @@ export class MyStack extends cdk.Stack {
       },
     });
 
+    const userPool = new cognito.UserPool(this, 'medium-cognito-userpool', {
+      userPoolName: 'medium-cognito-userpool',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    userPool.addClient('medium-client-1', {
+      oAuth: {
+        flows: {
+          authorizationCodeGrant: true,
+          implicitCodeGrant: true
+        },
+        scopes: [OAuthScope.OPENID],
+        callbackUrls: ['https://www.google.com.au/'],
+      },
+      userPoolClientName: 'medium-client-1',
+      generateSecret: false,
+      authFlows: {
+        userPassword: true,
+        userSrp: true,
+      },
+      enableTokenRevocation: true,
+      preventUserExistenceErrors: true,
+      idTokenValidity: Duration.hours(1),
+      accessTokenValidity: Duration.hours(1),
+      refreshTokenValidity: Duration.days(30),
+    });
+
+    userPool.addDomain('CognitoDomain', {
+      cognitoDomain: {
+        domainPrefix: 'my-medium-example',
+      },
+    });
+
+    const cognitoUserPoolsAuthorizer = new apigateway.CognitoUserPoolsAuthorizer(this, 'medium-cognito-authoriser', {
+      cognitoUserPools: [userPool],
+    });
+
+    const authorizer = {
+      authorizer: cognitoUserPoolsAuthorizer,
+      authorizationType: apigateway.AuthorizationType.COGNITO,
+    };
+
+    new cognito.CfnUserPoolUser(this, 'medium-admin-user', {
+      userPoolId: userPool.userPoolId,
+      forceAliasCreation: false,
+      userAttributes: [
+        {
+          name: 'email',
+          value: 'medium@example.com',
+        },
+      ],
+      username: 'medium-admin-user',
+    });
+
     const storesTable = new Table(this, 'stores-table', {
       partitionKey: { name: 'id', type: AttributeType.STRING },
       billingMode: BillingMode.PAY_PER_REQUEST,
       tableClass: TableClass.STANDARD_INFREQUENT_ACCESS,
       tableName: 'stores',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
     const nodejsProps: NodejsFunctionProps = {
@@ -38,11 +91,11 @@ export class MyStack extends cdk.Stack {
       environment: {
         STORE_PRIMARY_KEY: 'id',
         STORE_TABLE_NAME: storesTable.tableName,
-      }
+      },
     };
 
     const getOneLambda = new NodejsFunction(this, 'getOneStoreFunction', {
-      entry: join(__dirname, '..', 'lambda',  'get-one.ts'),
+      entry: join(__dirname, '..', 'lambda', 'get-one.ts'),
       ...nodejsProps,
     });
 
@@ -55,7 +108,7 @@ export class MyStack extends cdk.Stack {
       entry: join(__dirname, '..', 'lambda', 'create.ts'),
       ...nodejsProps,
     });
-    
+
     const updateOneLambda = new NodejsFunction(this, 'updateStoreFunction', {
       entry: join(__dirname, '..', 'lambda', 'update-one.ts'),
       ...nodejsProps,
@@ -66,25 +119,24 @@ export class MyStack extends cdk.Stack {
       ...nodejsProps,
     });
 
-    [createOneLambda, getAllLambda, getOneLambda, updateOneLambda, deleteOneLambda]
-    .forEach(i => storesTable.grantReadWriteData(i));
+    [createOneLambda, getAllLambda, getOneLambda, updateOneLambda, deleteOneLambda].forEach((i) => storesTable.grantReadWriteData(i));
 
-    const getAllIntegration = new LambdaIntegration(getAllLambda, {proxy: true});
-    const createOneIntegration = new LambdaIntegration(createOneLambda, {proxy: true});
-    const getOneIntegration = new LambdaIntegration(getOneLambda, {proxy: true});
-    const updateOneIntegration = new LambdaIntegration(updateOneLambda, {proxy: true});
-    const deleteOneIntegration = new LambdaIntegration(deleteOneLambda, {proxy: true});
+    const getAllIntegration = new LambdaIntegration(getAllLambda, { proxy: true });
+    const createOneIntegration = new LambdaIntegration(createOneLambda, { proxy: true });
+    const getOneIntegration = new LambdaIntegration(getOneLambda, { proxy: true });
+    const updateOneIntegration = new LambdaIntegration(updateOneLambda, { proxy: true });
+    const deleteOneIntegration = new LambdaIntegration(deleteOneLambda, { proxy: true });
 
     const store = api.root.addResource('store');
 
-    store.addMethod('POST', createOneIntegration);
-    store.addMethod('GET', getAllIntegration);
+    store.addMethod('POST', createOneIntegration, { ...authorizer });
+    store.addMethod('GET', getAllIntegration, { ...authorizer });
 
     const singleStore = store.addResource('{id}');
-    singleStore.addMethod('GET', getOneIntegration);
-    singleStore.addMethod('PATCH', updateOneIntegration);
-    singleStore.addMethod('DELETE', deleteOneIntegration);
-    
-    new cdk.CfnOutput(this, 'apiUrl', {value: api.url});
+    singleStore.addMethod('GET', getOneIntegration, { ...authorizer });
+    singleStore.addMethod('PATCH', updateOneIntegration, { ...authorizer });
+    singleStore.addMethod('DELETE', deleteOneIntegration, { ...authorizer });
+
+    new cdk.CfnOutput(this, 'apiUrl', { value: api.url });
   }
 }
